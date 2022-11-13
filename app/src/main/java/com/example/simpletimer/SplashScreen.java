7220ef1915a41be14ec32878b93f8a7ab126610e
aldiarsyah81 <aldiarsyah81@gmail.com>
2022-11-13 21:06:33 +0700

    Simple Timer(Final Design)

---
+++ app/src/main/java/com/example/simpletimer/SplashScreen.java
@@ -0,0 +1,29 @@
+package com.example.simpletimer;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+
+public class SplashScreen extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_splash_screen);
+
+        Thread thread = new Thread() {
+            public void run(){
+                try {
+                    sleep(2000);
+                }catch (InterruptedException e){
+                    e.printStackTrace();
+                }finally {
+                    startActivity(new Intent(SplashScreen.this, MainActivity.class));
+                    finish();
+                }
+            }
+        };
+        thread.start();
+    }
+}
